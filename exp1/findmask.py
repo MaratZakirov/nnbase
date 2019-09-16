c81fb760d34b0c699c6968ce890a1094e80cb23a
MaratZakirov <marat61@gmail.com>
2019-09-16 18:11:18 +0300

    First experiment done, optimal structure is found

--- exp1/findmask.py
+++ exp1/findmask.py
@@ -12,30 +12,40 @@ class MyNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv = nn.Conv2d(1, 1, kernel_size=5, padding=1)
-        self.end  = nn.Linear(in_features=1 * 18 * 18, out_features=1)
         self.sigmoid = nn.Sigmoid()
+        self.pool = nn.MaxPool2d(18, stride=2)
 
     def forward(self, x):
+        #torchvision.transforms.ToPILImage()(x[0, 0].detach().cpu()).resize((800, 800)).show()
+
         x = self.conv(x)
-        x = self.end(x.reshape(-1, 1 * 18 * 18))
+
+        #torchvision.transforms.ToPILImage()(x[0, 0].detach().cpu()).resize((800, 800)).show()
+        #torchvision.transforms.ToPILImage()(self.conv.weight[0, 0].detach().cpu()).resize((800, 800)).show()
+
+        x = self.pool(x)
         x = self.sigmoid(x)
-        return x
+        return x.reshape(-1, 1)
 
 def ShowTensor(x):
     assert len(x.shape) == 2
+    print(x.max(), x.min())
     x = (x - x.min()) / (x.max() - x.min())
     torchvision.transforms.ToPILImage()(x).resize((800, 800)).show()
 
 def GenRandomData(batch_size=64):
-    x = torch.rand(batch_size, 1, 20, 20)
-    y = torch.FloatTensor(batch_size).zero_()
+    x = torch.rand(batch_size, 1, 20, 20) * 0.5
+    y = torch.FloatTensor(batch_size, 1).zero_()
     for i in range(batch_size):
         pos_prob = np.random.uniform(low=0, high=1.0)
 
-        if pos_prob < 0.3:
-            x[i, 0, 2 : 10, 4] = 1.0
-            x[i, 0, 2, 4 : 12] = 1.0
-            y[i] = 1.0
+        if pos_prob < 0.5:
+            x_sh = np.random.randint(20 - 5)
+            y_sh = np.random.randint(20 - 5)
+
+            x[i, 0, y_sh: 4 + y_sh, x_sh] = 1.0
+            x[i, 0, y_sh, x_sh : 4 + x_sh] = 1.0
+            y[i, 0] = 1.0
 
         #torchvision.transforms.ToPILImage()(x[i]).resize((500, 500)).show()
 
@@ -46,10 +56,12 @@ if torch.cuda.is_available():
     model = model.cuda()
 
 loss = torch.nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.0, lr=0.0001)
+#loss = torch.nn.MSELoss()
+
+optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.0, lr=0.001)
 lr_scheduller = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
 
-for epoch in range(10000):
+for epoch in range(1000):
     x, y = GenRandomData()
 
     if torch.cuda.is_available():
@@ -69,6 +81,4 @@ for epoch in range(10000):
 
     print("Epoch: ", epoch, Loss.item())
 
-torchvision.transforms.ToPILImage()(x[0].detach().cpu()).resize((800, 800)).show()
-torchvision.transforms.ToPILImage()(y[0].detach().cpu()).resize((800, 800)).show()
-torchvision.transforms.ToPILImage()(out[0].detach().cpu()).resize((800, 800)).show()
+ShowTensor(model.conv.weight[0, 0].detach().cpu())
